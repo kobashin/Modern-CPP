ecf5f6c8b50838b0f7f085144c2af47dd860b16a
kobayashi-laptop <shin5884genuine@outlook.jp>
2025-02-23 10:47:32 +0000

    push_back_all:receive variadic arguments

--- test_vector.cpp
+++ test_vector.cpp
@@ -3,17 +3,32 @@
 #include <iostream>
 using namespace std;
 
-void output(vector<int>& v) {
+template <class T>
+void output(vector<T>& c) {
     cout << '{';
-    for (int x : v) {cout << x << ' ';}
+    for (T x : c) {cout << x << ' ';}
     cout << '}' << endl;
 }
 
-void push_back_all(const vector<int>& v, int a) {
+template <class T>
+void push_back_all(vector<T>& v, T a) {
     v.push_back(a);
 }
 
+template <class T, class... Ts>
+void push_back_all(vector<T>& v, T v1, Ts...tail){
+    v.push_back(v1);
+    push_back_all(v, tail...);
+}
+
 int main() {
+    vector<double> f = {1.2, 1.51, 2.01, 0.01};
     vector<int> v = {1, 3, 7};
+    output(f);
+    output(v);
+
+    push_back_all(f, 3.14, 2.78, 0.123);
+    push_back_all(v, 4, 12, 0);
+    output(f);
     output(v);
 }
