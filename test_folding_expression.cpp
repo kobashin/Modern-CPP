226418d59e75d8d4961dd9f1af71d9ce8dcf95bb
genuine <shin5884genuine@outlook.com>
2025-02-27 10:50:11 +0900

    fixed test_folding_expression.cpp

--- test_folding_expression.cpp
+++ test_folding_expression.cpp
@@ -1,5 +1,30 @@
+// https://cpprefjp.github.io/lang/cpp17/folding_expressions.html
+
 #include <vector>
+#include <string>
 #include <iostream>
+
+template <typename... Args>
+auto sum(Args... args) {
+    return (args + ...);
+}
+
+template <typename... Args>
+auto sum_with_fallback(Args... args) {
+    return (args + ... + 0);
+}
+
+template <typename... Args>
+bool all(Args... args) {
+    // 短絡評価をうまく使うには、左畳み込みの方が効率が良い？
+    return (... && args);
+    /// return (args && ...);
+}
+
+template <typename... Args>
+void print_all(std::ostream& os, Args... args) {
+    (os << ... << args);
+}
 
 template <class T>
 T square(T t) {
@@ -9,8 +34,22 @@ T square(T t) {
 template <class... T>
 auto summed_square(T... Args) {
     return (... + square(Args));
+    // return (... + (Args * Args));    // OK
 }
 
 int main() {
+    std::cout << "-auto sum(Args... args)-" << std::endl;
+    sum(1, 2, 3, 4);
+    
+    std::cout << "-auto sum_with_fallback(Args... args)-" << std::endl;
+    sum();
+    
+    std::cout << "-bool all(Args... args)-" << std::endl;
+    all(true, true, true);
+    
+    std::cout << "-void print_all(std::ostream& os, Args... args)-" << std::endl;
+    print_all(std::cout, 1, 2, 3, '\n');
+
+    std::cout << "-summed_square(3, 4)-" << std::endl;
     std::cout << summed_square(3, 4);    // Answer is 25.
 }
