0184b1769d2fb8b359a4688415d8ca9f59fbf2de
kobayashi-laptop <shin5884genuine@outlook.jp>
2025-04-12 08:37:42 +0000

    solved q026

--- q026.cpp
+++ q026.cpp
@@ -9,7 +9,25 @@
 #include <iostream>
 #include <string>
 
+void joinString(std::initializer_list<std::string> list, char c) {
+    /* 
+    for (std::string str : list) {
+        std::cout << str << c;
+    }
+    std::cout << std::endl; 
+    */
+    auto itr_begin = list.begin();
+    auto itr_end = list.end();
+
+    while ((itr_begin + 1) != itr_end) {
+        std::cout << *itr_begin << c;
+        ++itr_begin;
+    }
+    std::cout << *itr_begin << std::endl;
+}
+
 int main() {
+    /*
     std::string s;
     char d;
 
@@ -17,6 +35,7 @@ int main() {
     std::getline(std::cin, s);
     
     std::cout << "Enter your delimiter" << std::endl;
+    */
     
     // Cannot receive white space as a char...
     // std::cin >> d;
@@ -27,10 +46,16 @@ int main() {
 
         https://cpprefjp.github.io/reference/ios/noskipws.html
     */
+    
+    /*
     std::cin >> std::noskipws >> d;
 
     
     std::cout << "String from terminal : " << s << std::endl;
     std::cout << "Delimiter from terminal : '" << d  << "'" << std::endl;
+    */
+    
+    joinString({"Pen", "pineapple", "apple", "pen."}, ' ');
+    
     return 0;
 }
