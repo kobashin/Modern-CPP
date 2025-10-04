a5057cf228d65a2ce4f71b2ebdb74c5d229dc289
genuine <shin5884genuine@outlook.com>
2025-10-04 13:47:02 +0900

    add delete() function to call destructor

--- design_pattern/reminder_test.cpp
+++ design_pattern/reminder_test.cpp
@@ -1,10 +1,17 @@
 #include "MorningReminder.hpp"
 #include "NightReminder.hpp"
+#include <iostream>
 
 int main(void) {
     MorningReminder* m = new MorningReminder;
     m->remind();
+    delete(m);
+
+    std::cout << std::endl;
 
     NightReminder* n = new NightReminder;
     n->remind();
+    delete(n);
+
+    return 0;
 }
